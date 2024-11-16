9b2c3927c2860337814087347957a5f9c2c1d63f
Blackoutburst <blackoutburst@hotmail.com>
2024-11-16 23:31:01 +0100

    Faster monotype gen mesh

--- src/main/kotlin/dev/blackoutburst/game/world/Chunk.kt
+++ src/main/kotlin/dev/blackoutburst/game/world/Chunk.kt
@@ -91,6 +91,97 @@ class Chunk(
     }
 
     private fun genMesh() {
+        if (isMonoType()) {
+            val blockType = BlockType.getByID(blocks[0])
+            val t1 = blockType.textures[0]
+            val t2 = blockType.textures[1]
+            val t3 = blockType.textures[2]
+            val t4 = blockType.textures[3]
+            val t5 = blockType.textures[4]
+            val t6 = blockType.textures[5]
+
+            // TOP
+            vertices.add(packData(0, 16, 0, 0, 0, 0, t1))
+            vertices.add(packData(16, 16, 0, 16, 0, 0, t1))
+            vertices.add(packData(16, 16, 16, 16, 16, 0, t1))
+            vertices.add(packData(0, 16, 16, 0, 16, 0, t1))
+
+            indices.add(0)
+            indices.add(2)
+            indices.add(1)
+            indices.add(0)
+            indices.add(3)
+            indices.add(2)
+
+            // FRONT
+            vertices.add(packData(0, 0, 0, 16, 16, 1, t2))
+            vertices.add(packData(16, 0, 0, 0, 16, 1, t2))
+            vertices.add(packData(16, 16, 0, 0, 0, 1, t2))
+            vertices.add(packData(0, 16, 0, 16, 0, 1, t2))
+
+            indices.add(4)
+            indices.add(6)
+            indices.add(5)
+            indices.add(4)
+            indices.add(7)
+            indices.add(6)
+
+            // BACK
+            vertices.add(packData(0, 0, 16, 0, 16, 2, t3))
+            vertices.add(packData(16, 0, 16, 16, 16, 2, t3))
+            vertices.add(packData(16, 16, 16, 16, 0, 2, t3))
+            vertices.add(packData(0, 16, 16, 0, 0, 2, t3))
+
+            indices.add(9)
+            indices.add(10)
+            indices.add(8)
+            indices.add(10)
+            indices.add(11)
+            indices.add(8)
+
+            // LEFT
+            vertices.add(packData(0, 0, 0, 16, 16, 3, t4))
+            vertices.add(packData(0, 16, 0, 16, 0, 3, t4))
+            vertices.add(packData(0, 16, 16, 0, 0, 3, t4))
+            vertices.add(packData(0, 0, 16, 0, 16, 3, t4))
+
+            indices.add(12)
+            indices.add(14)
+            indices.add(13)
+            indices.add(12)
+            indices.add(15)
+            indices.add(14)
+
+            // RIGHT
+            vertices.add(packData(16, 0, 0, 0, 16, 4, t5))
+            vertices.add(packData(16, 16, 0, 0, 0, 4, t5))
+            vertices.add(packData(16, 16, 16, 16, 0, 4, t5))
+            vertices.add(packData(16, 0, 16, 16, 16, 4, t5))
+
+            indices.add(17)
+            indices.add(18)
+            indices.add(16)
+            indices.add(18)
+            indices.add(19)
+            indices.add(16)
+
+            // BOTTOM
+            vertices.add(packData(0, 0, 0, 0, 16, 5, t6))
+            vertices.add(packData(16, 0, 0, 16, 16, 5, t6))
+            vertices.add(packData(16, 0, 16, 16, 0, 5, t6))
+            vertices.add(packData(0, 0, 16, 0, 0, 5, t6))
+
+            indices.add(21)
+            indices.add(22)
+            indices.add(20)
+            indices.add(22)
+            indices.add(23)
+            indices.add(20)
+
+            indexCount = indices.size
+            return
+        }
+
         var offset = 0
         block@for (i in 0 until 4096) {
             val block = blocks[i]
@@ -102,7 +193,7 @@ class Chunk(
             val y = internalXYZIndexObject.y
             val z = internalXYZIndexObject.z
 
-            val faces = getVisibleFaces(internalXYZIndexObject)
+            val faces = getVisibleFaces()
 
             face@for (j in 0 until 6) {
                 if (!faces[j]) continue@face
@@ -216,31 +307,31 @@ class Chunk(
 
     fun xyzToIndex(x: Int, y: Int, z: Int): Int = x + 16 * (y + 16 * z)
 
-    private fun getVisibleFaces(position: Vector3i): BooleanArray {
+    private fun getVisibleFaces(): BooleanArray {
         val faces = BooleanArray(6)
 
         // TOP
-        val b1 = getBlockAt(position.x, position.y + 1, position.z)
+        val b1 = getBlockAt(internalXYZIndexObject.x, internalXYZIndexObject.y + 1, internalXYZIndexObject.z)
         faces[0] = (b1 == null || b1 == BlockType.AIR || b1.transparent)
 
         // FRONT
-        val b2 = getBlockAt(position.x, position.y, position.z - 1)
+        val b2 = getBlockAt(internalXYZIndexObject.x, internalXYZIndexObject.y, internalXYZIndexObject.z - 1)
         faces[1] = (b2 == null || b2 == BlockType.AIR || b2.transparent)
 
         // BACK
-        val b3 = getBlockAt(position.x, position.y, position.z + 1)
+        val b3 = getBlockAt(internalXYZIndexObject.x, internalXYZIndexObject.y, internalXYZIndexObject.z + 1)
         faces[2] = (b3 == null || b3 == BlockType.AIR || b3.transparent)
 
         // LEFT
-        val b4 = getBlockAt(position.x - 1, position.y, position.z)
+        val b4 = getBlockAt(internalXYZIndexObject.x - 1, internalXYZIndexObject.y, internalXYZIndexObject.z)
         faces[3] = (b4 == null || b4 == BlockType.AIR || b4.transparent)
 
         // RIGHT
-        val b5 = getBlockAt(position.x + 1, position.y, position.z)
+        val b5 = getBlockAt(internalXYZIndexObject.x + 1, internalXYZIndexObject.y, internalXYZIndexObject.z)
         faces[4] = (b5 == null || b5 == BlockType.AIR || b5.transparent)
 
         // BOTTOM
-        val b6 = getBlockAt(position.x, position.y - 1, position.z)
+        val b6 = getBlockAt(internalXYZIndexObject.x, internalXYZIndexObject.y - 1, internalXYZIndexObject.z)
         faces[5] = (b6 == null || b6 == BlockType.AIR || b6.transparent)
 
         return faces
@@ -249,6 +340,16 @@ class Chunk(
     private fun getBlockAt(x: Int, y: Int, z: Int): BlockType? {
         if (x < 0 || x > 15 || y < 0 || y > 15 || z < 0 || z > 15) return null
         return BlockType.getByID(blocks[xyzToIndex(x, y, z)])
+    }
+
+    private fun isMonoType(): Boolean {
+        var previous = this.blocks[0]
+        for (i in 1 until 4096) {
+            val current = this.blocks[i]
+            if (previous != current) return false
+            previous = current
+        }
+        return true
     }
 
     companion object {

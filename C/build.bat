32d4021e61bd25b97c4275c77a948a56ce4b70cb
Blackoutburst <blackoutburst@hotmail.com>
2025-01-13 22:07:08 +0100

    fixed error

--- C/build.bat
+++ C/build.bat
@@ -1,1 +1,1 @@
-gcc src/utils/*.c src/*.c src/graphics/*.c src/window/*.c src/world/*.c src/network/*.c -Iincludes -Llibs -lglfw3 -lglew32 -lopengl32 -lws2_32 -W -Wall -Wextra -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-pointer-sign
+gcc src/utils/*.c src/*.c src/graphics/*.c src/window/*.c src/world/*.c src/network/*.c -Iincludes -Llibs -lglfw3 -lglew32 -lopengl32 -lws2_32 -W -Wall -Wextra -Wno-implicit-fallthrough -Wno-unused-parameter
--- C/includes/utils/types.h
+++ C/includes/utils/types.h
@@ -4,12 +4,12 @@
 typedef uint64_t U64;
 typedef uint32_t U32;
 typedef uint16_t U16;
-typedef uint8_t U8;
+typedef unsigned char U8;
 
 typedef int64_t I64;
 typedef int32_t I32;
 typedef int16_t I16;
-typedef int8_t I8;
+typedef char I8;
 
 typedef float F32;
 typedef double F64;

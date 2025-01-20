88fbc6f4bcbc5af323f49c471c80ee8e55898947
Blackoutburst <blackoutburst@hotmail.com>
2025-01-20 23:45:16 +0100

    updfated osx build

--- C/build-osx.sh
+++ C/build-osx.sh
@@ -5,4 +5,4 @@ src/graphics/*.c \
 src/window/*.c \
 src/world/*.c \
 src/network/*.c \
--Iincludes -L/opt/homebrew/lib -pthread -lglfw -framework Cocoa -framework OpenGL -framework IOKit -Wno-deprecated-declarations -W -Wall -Wextra -Wno-unused-parameter
+-Iincludes -L/opt/homebrew/lib -pthread -lglfw -framework Cocoa -framework OpenGL -framework IOKit -Wno-deprecated-declarations -W -Wall -Wextra -Wno-unused-parameter -g3 -fno-omit-frame-pointer -fsanitize=address

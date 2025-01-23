2aa675653d36dcdcb5a7c0907c86fc572d9208a2
Blackoutburst <blackoutburst@hotmail.com>
2025-01-23 18:50:09 +0100

    code refactor

--- C/src/main.c
+++ C/src/main.c
@@ -22,53 +22,7 @@
 #include "network/encoder.h"
 #include "world/vaoQueue.h"
 #include "core/camera.h"
-
-#if defined(_WIN32) || defined(_WIN64)
-void calculateFPS() {
-    static I32 frameCount = 0;
-    static F64 elapsedTime = 0.0;
-    static F64 lastTime = 0.0;
-    
-    LARGE_INTEGER frequency, currentTime;
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&currentTime);
-
-    F64 now = (F64)currentTime.QuadPart / frequency.QuadPart;
-    elapsedTime += now - lastTime;
-    lastTime = now;
-
-    frameCount++;
-
-    if (elapsedTime >= 1.0) {
-        printf("FPS: %d\n", frameCount);
-        frameCount = 0;
-        elapsedTime = 0.0;
-    }
-}
-#else
-void calculateFPS() {
-    static I32 frameCount = 0;
-    static F64 elapsedTime = 0.0;
-    static struct timespec lastTime = {0, 0};
-
-    struct timespec currentTime;
-    clock_gettime(CLOCK_MONOTONIC, &currentTime);
-
-    F64 now = currentTime.tv_sec + (currentTime.tv_nsec / 1e9);
-    F64 last = lastTime.tv_sec + (lastTime.tv_nsec / 1e9);
-    elapsedTime += now - last;
-    lastTime = currentTime;
-
-    frameCount++;
-
-    if (elapsedTime >= 1.0) {
-        printf("FPS: %d\n", frameCount);
-        frameCount = 0;
-        elapsedTime = 0.0;
-    }
-}
-#endif
-
+#include "utils/framerate.h"
 
 #if defined(_WIN32) || defined(_WIN64)
 void sendPosition(F32 x, F32 y, F32 z, F32 yaw, F32 pitch) {
